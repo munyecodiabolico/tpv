9d999e8605ea3f52c1c3294d5ffe95f5bbfb52f9
Peverus <99191027+Peverus@users.noreply.github.com>
2022-07-12 19:54:55 +0200

    Cambios 12-07

--- admin-mesas.php
+++ admin-mesas.php
@@ -23,53 +23,237 @@
             </div>
             <div class="col-12 mt-5">
                 <section>
-                    <div class="row mb-5">
+                    <div class="row">
+                        <div class="col d-flex justify-content-end">
+                        <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#addArticle">+ Añadir mesa</button>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
                         <div class="col-12">
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Ubicación</th>
-                                    <th scope="col">Nº Comensales</th>
-                                    <th scope="col">Opciones</th>
+                                    <th scope="col" class="numero-mesa">Nº mesa</th>
+                                    <th scope="col" class="ubicacion">Ubicación</th>
+                                    <th scope="col" class="comensales">Nº Comensales</th>
+                                    <th scope="col" class="opciones">Opciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                    <th scope="row">1</th>
-                                    <td>Terraza</td>
-                                    <td>4</td>
-                                    <td>
-                                    <button type="button" class="btn btn-secondary">
-                                        <i class="fa fa-edit"></i>
-                                    </button>
-                                    <button type="button" class="btn btn-secondary">
-                                        <i class="fa fa-trash"></i>
-                                    </button>
-                                    </td>
+                                        <th scope="row" class="numero-mesa">1</th>
+                                        <td class="ubicacion">Local</td>
+                                        <td class="comensales">4</td>
+                                        <td class="opciones">
+                                            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#editArticle">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteArticle">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        </td>
                                     </tr>
                                     <tr>
-                                    <th scope="row">2</th>
-                                    <td>Local</td>
-                                    <td>8</td>
-                                    <td>@fat</td>
+                                        <th scope="row" class="numero-mesa">2</th>
+                                        <td class="ubicacion">Terraza</td>
+                                        <td class="comensales">6</td>
+                                        <td class="opciones">
+                                            <button type="button" class="btn btn-success">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-danger">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        </td>
                                     </tr>
                                     <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Local</td>
-                                    <td>@6</td>
+                                        <th scope="row" class="numero-mesa">3</th>
+                                        <td class="ubicacion">Terraza</td>
+                                        <td class="comensales">5</td>
+                                        <td class="opciones">
+                                            <button type="button" class="btn btn-success">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-danger">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="numero-mesa">4</th>
+                                        <td class="ubicacion">Local</td>
+                                        <td class="comensales">2</td>
+                                        <td class="opciones">
+                                            <button type="button" class="btn btn-success">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-danger">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row" class="numero-mesa">5</th>
+                                        <td class="ubicacion">Terraza</td>
+                                        <td class="comensales">8</td>
+                                        <td class="opciones">
+                                            <button type="button" class="btn btn-success">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                            <button type="button" class="btn btn-danger">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
+                    <div class="row justify-content-center">
+                        <div class="col">
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination justify-content-center">
+                                    <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                    </li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
                 </section>
             </div>
-            
-
 
         </div>
     </div>
+    <!-- Modal ADD ARTICLE-->
+    <div>
+        <div  id="addArticle" class="modal fade" tabindex="-1" aria-labelledby="addArticleLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addArticleLabel">AÑADIR MESA Nº 4</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="numeroMesa" class="form-label">Correo electrónico</label>
+                            <input type="number" class="form-control" id="numeroMesa" placeholder="4" disabled>
+                        </div>
+                        <div class="mb-3">
+                            <label for="ubicacion" class="form-label">Ubicación</label>
+                            <select class="form-select" aria-label="Default select example" id="ubicacion">
+                                <option selected>Selecciona ubicación</option>
+                                <option value="1">Local</option>
+                                <option value="2">Terraza</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="ubicacion" class="form-label">Número de comensales</label>
+                            <select class="form-select" aria-label="Default select example" id="ubicacion">
+                                <option selected>Selecciona número de comensales</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </select>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button type="button" class="btn btn-secondary mt-3 me-2" data-bs-dismiss="modal">CERRAR</button>
+                            <button type="submit" class="btn btn-primary mt-3">CONFIRMAR</button>
+                        </div>
+                    </form>
+                
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Modal EDIT ARTICLE-->
+    <div>
+        <div  id="editArticle" class="modal fade" tabindex="-1" aria-labelledby="editArticleLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editArticleLabel">EDITAR MESA Nº 1</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="ubicacion" class="form-label">Ubicación</label>
+                            <select class="form-select" aria-label="Default select example" id="ubicacion">
+                                <option selected>Selecciona ubicación</option>
+                                <option value="1">Local</option>
+                                <option value="2">Terraza</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="ubicacion" class="form-label">Número de comensales</label>
+                            <select class="form-select" aria-label="Default select example" id="ubicacion">
+                                <option selected>Selecciona número de comensales</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </select>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button type="button" class="btn btn-secondary mt-3 me-2" data-bs-dismiss="modal">CERRAR</button>
+                            <button type="submit" class="btn btn-primary mt-3">CONFIRMAR</button>
+                        </div>
+                    </form>
+                
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Modal DELETE ARTICLE-->
+    <div>
+        <div id="deleteArticle" class="modal fade" tabindex="-1" aria-labelledby="deleteArticleLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteArticleLabel">ELIMINAR MESA Nº 1</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p class="text-center text-muted">Está a punto de borrar el pedido sin ser cobrado. ¿Está completamente seguro de realizar esta acción?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">CERRAR</button>
+                    <button type="button" class="btn btn-primary">ELIMINAR</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script type="module" src="dist/main.js"></script>
 </body>
